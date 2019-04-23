5144f101f64625d60038ce4c1bbf4082a06464bd
Alexsandre <Alexsandrebr@hotmail.com.br>
2019-04-23 13:40:02 -0300

    Glider Fix

--- CreativerseMod/Patches/Patch_Glider.cs
+++ CreativerseMod/Patches/Patch_Glider.cs
@@ -9,34 +9,6 @@ namespace CreativerseMod.Patches
     {
         public static bool Prefix(Equipment __instance, EntityComponentMessage msg)
         {
-            if(LoaderConfig.Instance.EnableGlider)
-            {
-                if(msg.GetType() == typeof(EquipmentEnableFeature))
-                {
-                    EquipmentEnableFeature _msg = (EquipmentEnableFeature)msg;
-
-                    switch(_msg.Feature)
-                    {
-                        case EquipmentFeature.Glider:
-                        {
-                            __instance.GliderOn = true;
-                            break;
-                        }
-                        case EquipmentFeature.Light:
-                        {
-                            __instance.LightOn = true;
-                            break;
-                        }
-                    }
-                    return false;
-                }
-                return true;
-            }  
-            return true;
-        }
-
-        public static void Postfix(User __instance)
-        {
             
         }
     }
